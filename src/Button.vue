f112f4d8bb664c876b8a5041c70c6fbbc087f465
hzj5016 <hzj5016@gmail.com>
2019-09-02 19:50:30 +0800

    添加按钮点击事件

--- src/Button.vue
+++ src/Button.vue
@@ -1,51 +1,84 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
-    <g-icon :name="icon" class="icon" v-if="icon && !loading"></g-icon>
-    <g-icon name="loading" class="loading icon" v-if="loading"></g-icon>
+  <button class="h-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+    <h-icon :name="icon" v-if="icon" :class="{loading: loading}"></h-icon>
     <div class="content">
       <slot></slot>
     </div>
   </button>
 </template>
+
 <script>
-  export default {
-    props: {
-      icon: {},
-      loading: {
-        type: Boolean,
-        default: false
-      },
-      iconPosition: {
-        type: String,
-        default: 'left',
-        validator (value) {
-          return value === 'left' || value === 'right'
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(val) {
+        if(val !== 'left' && val !== 'right') {
+          return false
+        }else {
+          return true
         }
       }
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
   }
+};
 </script>
-<style lang="scss">
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
+
+<style lang="scss" scoped>
+@keyframes spin {
+  0% {transform: rotate(0deg)}
+  100% {transform: rotate(360deg)}
+}
+.h-button {
+  font-size: var(--font-size);
+  background: var(--button-bg);
+  padding: 0 1em;
+  height: var(--button-height);
+  color: var(--color);
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color);
+    background: #eee;
+    transition: background-color 0.6s;
   }
-  .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
-    border-radius: var(--border-radius); border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    display: inline-flex; justify-content: center; align-items: center;
-    vertical-align: middle;
-    &:hover { border-color: var(--border-color-hover); background-color: rgb(247, 240, 240);}
-    &:active { background-color: var(--button-active-bg); }
-    &:focus { outline: none; }
-    > .content { order: 2; }
-    > .icon { order: 1; margin-right: .2em; }
-    &.icon-right {
-      > .content { order: 1; }
-      > .icon { order: 2; margin-right: 0; margin-left: .2em;}
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  &.icon-left {
+    > .h-icon {
+      margin-right: 0.1em;
+      order: 1;
     }
-    .loading{
-      animation: spin 2s infinite linear;
+    > .content {
+      order: 2;
     }
   }
+  &.icon-right {
+    > .h-icon {
+      margin-right: 0;
+      margin-left: 0.1em;
+      order: 2;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+  .loading{
+    animation: spin 2s linear infinite;
+  }
+}
 </style>
--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import Button from './Button.vue'
 import Icon from './Icon'
 
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
+Vue.component('h-button', Button)
+Vue.component('h-icon', Icon)
 
 new Vue({
   data: {
-    loading1: null
+    loading1: false
   }
 }).$mount('#app')
